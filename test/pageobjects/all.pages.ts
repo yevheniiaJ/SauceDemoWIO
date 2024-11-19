98d9806196bfb4ae1f926184637698afd9691090
test tester <testert573@gmail.com>
2024-11-19 23:24:24 +0200

    test for footer

---
+++ test/pageobjects/all.pages.ts
@@ -0,0 +1,19 @@
+import { $ } from '@wdio/globals'
+import Page from './page.js';
+
+class AllPages extends Page {
+
+    public get twitterIcon () {
+        return $(`//a[@data-test='social-twitter']`)
+    }
+
+    public get facebookIcon () {
+        return $(`//a[@data-test='social-facebook']`)
+    }
+
+    public get linkedinIcon () {
+        return $(`//a[@data-test='social-linkedin']`)
+    }
+
+}
+export default  new AllPages();
