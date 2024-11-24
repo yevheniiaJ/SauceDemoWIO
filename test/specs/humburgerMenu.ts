052070c05034f76f76de74841a1c0a0524591cc0
test tester <testert573@gmail.com>
2024-11-25 00:00:22 +0200

    fixed 2 test, 1 test is added

--- test/specs/humburgerMenu.ts
+++ test/specs/humburgerMenu.ts
@@ -1,6 +1,6 @@
 import { expect } from '@wdio/globals'
 import LoginPage from '../pageobjects/login.page.ts';
-import HumburgerMenu from '../pageobjects/humburgerMenu.pages.ts'
+import HumburgerMenu from '../pageobjects/humburgerMenu.ts'
 import ProductsDetailsPage from '../pageobjects/productDetails.page.ts'
 import Products from '../pageobjects/products.page.js'
 
@@ -18,13 +18,12 @@ describe('Humburger Menu', () => {
         await browser.reloadSession();
     })
 
-    it.only('verify all items to be displayed ', async () => {
+    it('verify all items to be displayed ', async () => {
         await LoginPage.open()
         await LoginPage.login('standard_user', 'secret_sauce')
-        await browser.pause(4000);
         const elementsNumber = await Products.poductsList.length;
         await ProductsDetailsPage.productLink.click();
-        await ProductsDetailsPage.productName.waitForDisplayed({timeout: 4000});
+        await HumburgerMenu.menu.click();
         await HumburgerMenu.allItems.click();
         const elementsNumber1 = await  Products.poductsList.length;
         expect(elementsNumber).toStrictEqual(elementsNumber1);
@@ -34,11 +33,23 @@ describe('Humburger Menu', () => {
     it('verify app state to be resetted', async () => {
         await LoginPage.open()
         await LoginPage.login('standard_user', 'secret_sauce')
-        await browser.pause(1000);
         await Products.addToCard.click();
         await Products.cartBadge.waitForDisplayed({timeout: 3000});
+        await HumburgerMenu.menu.click();
         await HumburgerMenu.resetAppState.click();
         await expect (ProductsDetailsPage.cartBadge).not.toExist();
+    })
+
+
+    it('Verify Log out ', async () => {
+        await LoginPage.open();
+        await LoginPage.login('standard_user', 'secret_sauce');
+        const url1 = await browser.getUrl();
+        await HumburgerMenu.menu.click();
+        await HumburgerMenu.logOutLink.click();
+        const url = await browser.getUrl();
+        expect(url1).toEqual('https://www.saucedemo.com/inventory.html');
+        expect(url).toEqual('https://www.saucedemo.com/')
     })
 
 });
